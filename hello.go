25e6bd8d9d137864fbd771e224d2488a44b9e107
agrawalananya <ananya.agrawal@zopsmart.com>
2022-11-16 14:41:35 +0530

    slices

--- hello.go
+++ hello.go
@@ -1,33 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-
-	"github.com/agrawalananya/goTraining/module1"
-	"github.com/pborman/uuid"
-)
-
-const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
-
-const (
-	a = 37
-	b = 89
-)
+import "fmt"
 
 func main() {
-	uuidWithHyphen := uuid.NewRandom()
-	uuid := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
-	fmt.Println(uuid)
-	module1.Something()
-	for i := 0; i < len(sample); i++ {
-		fmt.Print(sample[i])
-	}
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
 
-	fmt.Println("\n", a, b)
+	// Slice the slice to give it zero length.
+	s = s[:0]
+	printSlice(s)
+
+	// Extend its length.
+	s = s[:4]
+	printSlice(s)
+
+	// Drop its first two values.
+	s = s[1:]
+	printSlice(s)
+
+	s = append(s, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	printSlice(s)
+
+	kb := [2]string{"Penn", "Teller"}
+	ab := &kb
+	ac := kb
+	ac[0] = "dhchdsvchds"
+	fmt.Println(*ab)
+	fmt.Println(ac)
 
 }
 
-func Some() {
-	fmt.Println("Hello")
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
